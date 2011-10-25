785586cbe2c89ab85b9f79af5eb183044f56c209
Christopher Swenor <cswenor@gmail.com>
2011-10-25 16:35:08 -0400

    Modified the gamePiece

--- src/convolutededge/android/battletrains/gamePiece.java
+++ src/convolutededge/android/battletrains/gamePiece.java
@@ -3,10 +3,82 @@
  */
 package convolutededge.android.battletrains;
 
+import java.util.ArrayList;
+
+
+
 /**
  * @author cswenor
  *
  */
 public abstract class gamePiece {
+    /**
+     * Simple class containing two integer values and a comparison function.
+     * There's probably something I should use instead, but this was quick and
+     * easy to build.
+     * 
+     */
+	private ArrayList<Coordinate> mCoordinates = new ArrayList<Coordinate>();
+	
+	
+    /**
+     * Given a ArrayList of coordinates, we need to flatten them into an array of
+     * ints before we can stuff them into a map for flattening and storage.
+     * 
+     * @param cvec : a ArrayList of Coordinate objects
+     * @return : a simple array containing the x/y values of the coordinates
+     * as [x1,y1,x2,y2,x3,y3...]
+     */
+    private int[] coordArrayListToArray(ArrayList<Coordinate> cvec) {
+        int count = cvec.size();
+        int[] rawArray = new int[count * 2];
+        for (int index = 0; index < count; index++) {
+            Coordinate c = cvec.get(index);
+            rawArray[2 * index] = c.x;
+            rawArray[2 * index + 1] = c.y;
+        }
+        return rawArray;
+    }
+    
+    
+    /**
+     * Given a flattened array of ordinate pairs, we reconstitute them into a
+     * ArrayList of Coordinate objects
+     * 
+     * @param rawArray : [x1,y1,x2,y2,...]
+     * @return a ArrayList of Coordinates
+     */
+    private ArrayList<Coordinate> coordArrayToArrayList(int[] rawArray) {
+        ArrayList<Coordinate> coordArrayList = new ArrayList<Coordinate>();
 
+        int coordCount = rawArray.length;
+        for (int index = 0; index < coordCount; index += 2) {
+            Coordinate c = new Coordinate(rawArray[index], rawArray[index + 1]);
+            coordArrayList.add(c);
+        }
+        return coordArrayList;
+    }
+    
+    private class Coordinate {
+        public int x;
+        public int y;
+
+        public Coordinate(int newX, int newY) {
+            x = newX;
+            y = newY;
+        }
+
+        public boolean equals(Coordinate other) {
+            if (x == other.x && y == other.y) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "Coordinate: [" + x + "," + y + "]";
+        }
+    }
+    
 }
